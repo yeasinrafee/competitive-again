7fa0a293576e282e5e719d8a572d15004033a5aa
yeasinrafee <mdyeasinrafi7@gmail.com>
2025-07-28 20:26:04 +0600

    Count nodes using recursion

--- Data structure/Tree/Binary Tree/binary_tree.cpp
+++ Data structure/Tree/Binary Tree/binary_tree.cpp
@@ -60,6 +60,43 @@ void level_order(Node* root){
 	}
 }
 
+Node *input_tree(){
+	int val;
+	cin>>val;
+
+	Node* root;
+
+	if(val == -1) root = NULL;
+	else root = new Node(val);
+
+	queue<Node *> q;
+	if(root) q.push(root);
+
+	while(!q.empty()){
+		Node* p = q.front();
+		q.pop();
+
+		int l, r;
+		cin>>l>>r;
+
+		Node *myleft, *myright;
+
+		if(l == -1) myleft = NULL;
+		else myleft = new Node(l);
+
+		if(r == -1) myright = NULL;
+		else myright = new Node(r);
+
+		p->left = myleft;
+		p->right = myright;
+
+		if(p->left) q.push(p->left);
+		if(p->right) q.push(p->right);
+	}
+
+	return root;
+}
+
 
 int main(){
 	
