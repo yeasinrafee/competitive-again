5106a963035ebd7cb237c325dc5e97e1900fc239
yeasinrafee <mdyeasinrafi7@gmail.com>
2025-08-04 09:14:22 +0600

    Insert in max heap

---
+++ Data structure/Heap/max_insertion.cpp
@@ -0,0 +1,30 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+int main(){
+	
+	int n;
+	cin>>n;
+	vector<int> v(n);
+	for(int i = 0; i < n; i++){
+		cin>>v[i];
+	}
+	int val;
+	cin>>val;
+	v.push_back(val);
+	int cur_idx = v.size() - 1;
+	while(cur_idx != 0){
+		int par_idx = (cur_idx - 1)/2;
+		if(v[par_idx] < v[cur_idx])
+			swap(v[par_idx], v[cur_idx]);
+		else
+			break;
+		cur_idx = par_idx;
+	}
+
+	for(int x: v){
+		cout<<x<<" ";
+	}
+
+	return 0;
+}
