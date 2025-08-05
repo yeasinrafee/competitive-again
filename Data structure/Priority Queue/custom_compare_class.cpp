a42197c8e2da22ddac73c2685ac683768d62b7bf
yeasinrafee <mdyeasinrafi7@gmail.com>
2025-08-05 23:10:51 +0600

    custom compare class for sorting the priority_queue

---
+++ Data structure/Priority Queue/custom_compare_class.cpp
@@ -0,0 +1,49 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+class Student{
+public:
+	string name;
+	int roll;
+	int marks;
+	Student(string name, int roll, int marks){
+		this->name = name;
+		this->roll = roll;
+		this->marks = marks;
+	}
+};
+
+class cmp{
+public: 
+	bool operator()(Student l, Student r){
+		// return l.marks > r.marks;
+		if(l.marks > r.marks)
+			return true;
+		else if(l.marks < r.marks)
+			return false;
+		else{
+			return l.roll > r.roll;
+		}
+	}
+};
+
+int main(){
+	
+	priority_queue<Student, vector<Student>, cmp> pq;
+	int n;
+	cin>>n;
+	for(int i = 0; i < n; i++){
+		string name;
+		int roll, marks;
+		cin>>name>>roll>>marks;
+		Student obj(name, roll, marks);
+		pq.push(obj);
+	}
+
+	while(!pq.empty()){
+		cout<<pq.top().name<<" "<<pq.top().roll<<" "<<pq.top().marks<<endl;
+		pq.pop();
+	}
+
+	return 0;
+}
