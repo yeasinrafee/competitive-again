24106972f5193291fe9f324ece0fa155740272c3
yeasinrafee <mdyeasinrafi7@gmail.com>
2025-08-17 19:48:47 +0600

    Directed and undirected adjacency graph

---
+++ Data structure/Graph/adjacency_matrix_undirected.cpp
@@ -0,0 +1,41 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+int main(){
+	
+	int n, e;
+	cin>>n>>e;
+	int adj_mat [n][n];
+
+	// for(int i = 0; i < e; i++){
+	// 	for(int j = 0; i < n; j++){
+	// 		adj_mat[i][j] = 0;
+	// 	}
+	// }
+
+	memset(adj_mat, 0, sizeof(adj_mat));		// make all the matrix value as 0
+
+	for(int i = 0; i < n; i++){
+		for(int j = 0; j < n; j++){
+			if(i == j){
+				adj_mat[i][j] = 1;
+			}
+		}
+	}
+
+	for(int i = 0; i < e; i++){
+		int a, b;
+		cin>>a>>b;
+		adj_mat[a][b] = 1;
+		adj_mat[b][a] = 1;
+	}
+
+	for(int i = 0; i < n; i++){
+		for(int j = 0; j < n; j++){
+			cout<<adj_mat[i][j]<<" ";
+		}
+		cout<<endl;
+	}
+	
+	return 0;
+}
---
+++ Data structure/Graph/graph.cpp
@@ -0,0 +1,9 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+int main(){
+	
+	cout<<"Hellow"<<endl;
+
+	return 0;
+}
