8f21ebfbd37649b5014c3a06fa90dec7c4a48a93
yeasinrafee <mdyeasinrafi7@gmail.com>
2025-08-21 23:08:12 +0600

    BFS

---
+++ Data structure/Graph/BFS.cpp
@@ -0,0 +1,40 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+vector<int> adj_list[1005];
+bool vis[1005];
+
+void bfs(int src){
+	queue<int> q;
+	q.push(src);
+	vis[src] = true;
+	while(!q.empty()){
+		// ber kore ana
+		int par = q.front();
+		q.pop();
+		// oi node ke niye kaaz
+		cout<<par<<" ";
+		// children gulo push kora
+		for(int child: adj_list[par]){
+			if(!vis[child]){
+			q.push(child);
+			vis[child] = true;
+		}
+		}
+	}
+}
+
+int main(){
+	int n, e;
+	cin>>n>>e;
+	while(e--){
+		int a, b;
+		cin>>a>>b;
+		adj_list[a].push_back(b);
+		adj_list[b].push_back(a);
+	}
+	memset(vis, false, sizeof(vis));
+	bfs(0);
+
+	return 0;
+}
