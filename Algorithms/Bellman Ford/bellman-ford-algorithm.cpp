fea80751e8a66b9ed00fbea95db7c9412d1cc77c
yeasinrafee <mdyeasinrafi7@gmail.com>
2025-09-02 23:25:06 +0600

    Bellman-Ford algorithm for negative loop graph

--- Algorithms/Bellman Ford/bellman-ford-algorithm.cpp
+++ Algorithms/Bellman Ford/bellman-ford-algorithm.cpp
@@ -18,15 +18,37 @@ int n, e;
 void bellman_ford(){
 	for(int i = 0; i < n - 1; i++){
 		for(auto ed: edge_list){
+			int a, b, c;
+			a = ed.a;
+			b = ed.b;
+			c = ed.c;
+
+			if(dis[a] != INT_MAX && dis[a] + c < dis[b])
+				dis[b] = dis[a] + c;
+		}
+	}
+
+	bool cycle = false;
+
+	for(auto ed: edge_list){
 		int a, b, c;
 		a = ed.a;
 		b = ed.b;
 		c = ed.c;
 
-		if(dis[a] != INT_MAX && dis[a] + c < dis[b])
-			dis[b] = dis[a] + c;
+		if(dis[a] != INT_MAX && dis[a] + c < dis[b]){
+			cycle = true;
+			break;
 		}
 	}
+
+	if(cycle)
+		cout<<"Negetive weighted cycle detected\n";
+	else{
+		for(int i = 0; i < n; i++){
+			cout<<i<<"->"<<dis[i]<<endl;
+		}
+	}	
 }
 
 int main(){
@@ -46,14 +68,7 @@ int main(){
 
 	dis[0] = 0;
 
-
 	bellman_ford();
-
-	for(int i = 0; i < n; i++){
-		cout<<i<<"->"<<dis[i]<<endl;
-	}
-
-
 
 	return 0;
 }
